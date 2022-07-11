d41a05c489e5d6cc40c7fbfdc3f0101569698b97
Toray-Life <stypmabichek@gmail.com>
2022-07-11 14:50:43 +0400

    Setting up the environment

---
+++ modules/@types/jsExtension.js
@@ -0,0 +1,14 @@
+//#region STRING
+String.prototype.startsWith = function (subString) {
+    return true;
+};
+//#endregion STRING
+//#region ARRAY
+//#endregion ARRAY
+var Java = /** @class */ (function () {
+    function Java() {
+    }
+    Java.type = function (str) { };
+    Java.from = function (any) { return any; };
+    return Java;
+}());
---
+++ modules/@types/jsExtension.ts
@@ -0,0 +1,30 @@
+
+declare interface String {
+	startsWith(subString:String):boolean;
+}
+
+declare interface Array<T> {
+	toString():string;
+}
+
+//#region STRING
+String.prototype.startsWith = function(subString:string){
+    return true;
+}
+
+
+//#endregion STRING
+
+//#region ARRAY
+
+
+
+//#endregion ARRAY
+
+
+
+abstract class Java {
+
+	static type(str:string):any{}
+	static from<T>(any: T[]):T[]{return any;}
+}
