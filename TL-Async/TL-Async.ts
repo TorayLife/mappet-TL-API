ad60ba14e4c17274f20fdecb3bfffae21c166bba
Toray-Life <stypmabichek@gmail.com>
2022-07-14 14:05:56 +0400

    Work with TL-Async.ts

--- TL-Async/TL-Async.ts
+++ TL-Async/TL-Async.ts
@@ -1,220 +1,94 @@
-/**
- * Execution support for simple hybrid (async/sync) script execution in nashorn.
- *
- * Extend your nashorn script engine with async support by evaluating this script prior to the actual script.
- * Then, wrap the actual script you would like to execute in a `exec(fn, timeout)` block with `fn` containing
- * it.
- *
- * Scripts may call {@link #async} to retrieve a callback (err, result) that needs to be
- * triggered before the overall script execution terminates.
- *
- * <strong>Warning: </strong> If {@link #async} is invoked multiple times, all callbacks
- * need to be fulfilled but only the last one will decide upon the actual execution result.
- *
- * The function will always return synchronously and indicate script errors by throwing them.
- *
- * @example async
- *
- * exec(function() {
- *   var done = async();
- *
- *   setTimeout(function() {
- *     done(null, 'YES IT WORKED');
- *   }, 200);
- * });
- *
- * @example sync
- *
- * exec(function() {
- *   throw new Error('FAILED');
- * });
- *
- *
- * @author Nico Rehwaldt<git_nikku@nixis.de>
- */
+abstract class Async {
+	static JTimer = Java.type('java.util.Timer');
+	static JTimerTask = Java.type('java.util.TimerTask');
+	static JPhaser = Java.type('java.util.concurrent.Phaser');
+	static JTimeUnit = Java.type('java.util.concurrent.TimeUnit');
+	static timer = new Async.JTimer('loop', false);
+	static phaser = new Async.JPhaser();
 
-(function(context) {
+	static results;
+	static tasks = {};
 
-	var Timer = Java.type('java.util.Timer');
-	var TimerTask = Java.type('java.util.TimerTask');
-	var Phaser = Java.type('java.util.concurrent.Phaser');
-	var TimeUnit = Java.type('java.util.concurrent.TimeUnit');
+	private static async() {
+		this.phaser.register();
 
-	var System = Java.type('java.lang.System');
-
-	var timer = new Timer('jsEventLoop', false);
-
-	/**
-	 * Our global synchronization barrier that we use to register async operations
-	 * and that awaits for all of these operations to finish.
-	 */
-	var phaser = new Phaser();
-
-
-	/** global execution results */
-	var results;
-
-	function eventLoopError(e) {
-		System.out.print('Exception in jsEventLoop ');
-		e.printStackTrace();
-	}
-
-	function async() {
-		phaser.register();
-
-		return function(err, result) {
-			results = { err: err, result: result };
-			phaser.arriveAndDeregister();
+		return function (err, result) {
+			this.results = {err: err, result: result};
+			this.phaser.arriveAndDeregister();
 		};
 	};
 
+	private static setTimeout(fn, millis) {
+		this.phaser.register();
 
-	/**
-	 * Executes the given function after the specified timeout.
-	 *
-	 * @param {Function} fn
-	 * @param {Number} [millis]
-	 *
-	 * @return {Object} timeout handle
-	 */
-	function setTimeout(fn, millis) {
-		phaser.register();
-
-		var task = new TimerTask({
-			run: function() {
+		let task = new Async.JTimerTask({
+			run: function () {
 				try {
 					fn();
-				} catch(e) {
-					eventLoopError(e);
 				} finally {
-					phaser.arriveAndDeregister();
+					Async.phaser.arriveAndDeregister();
 				}
-			}
+			},
 		});
 
-		timer.schedule(task, millis || 0);
+		this.timer.schedule(task, millis || 0);
 
 		return task;
 	}
 
+	private static setInterval(fn, millis) {
+		this.phaser.register();
 
-	/**
-	 * Clear timeout previously created via {@link #setTimeout}.
-	 *
-	 * @param {Object} task timeout handle
-	 */
-	function clearTimeout(task) {
-		if (task.cancel()) {
-			phaser.arriveAndDeregister();
-		}
-	}
-
-	/**
-	 * Executes the given function with a fixed time delay.
-	 *
-	 * @param {Function} fn
-	 * @param {Number} [millis]
-	 *
-	 * @return {Object} timeout handle
-	 */
-	function setInterval(fn, millis) {
-		phaser.register();
-
-		var task = new TimerTask({
-			run: function() {
-				try {
-					fn();
-				} catch (e) {
-					eventLoopError(e);
-				}
+		var task = new this.JTimerTask({
+			run: function (){
+				fn();
 			}
 		});
 
-		timer.scheduleAtFixedRate(task, millis, millis);
+		this.timer.scheduleAtFixedRate(task, millis, millis);
 
 		return task;
 	}
 
-
-	/**
-	 * Clear interval previously created via {@link #setInterval}.
-	 *
-	 * @param {Object} interval handle
-	 */
-	function clearInterval(task) {
+	private static clearInterval(task) {
 		if (task.cancel()) {
-			phaser.arriveAndDeregister();
+			Async.phaser.arriveAndDeregister();
 		}
 	}
 
-	/**
-	 * Execute a function (synchronously or asynchronously) and return the result
-	 * or throw the error that is returned by it.
-	 *
-	 * @param {Function} fn
-	 * @param {Number} [timeout=5000]
-	 */
-	function exec(fn, timeout) {
-		timeout = timeout || 5000;
+	public static setTask(taskName:string, fn:()=>any, millis:number = 0, repeat:boolean = false){
 
-		phaser.register();
+		let done = this.async();
 
-		setTimeout(function() {
-			try {
-				results = { err: null, result: fn() };
-			} catch (e) {
-				results = { err: e };
-			}
-		}, 0);
-
-		try {
-			if (timeout < 0) {
-				phaser.awaitAdvanceInterruptibly(phaser.arrive());
-			} else {
-				phaser.awaitAdvanceInterruptibly(phaser.arrive(), timeout, TimeUnit.MILLISECONDS);
-			}
-		} catch (e) {
-			results = { err: new Error('execution timeout') };
-		} finally {
-			timer.cancel();
-		}
-
-		if (results.err) {
-			throw results.err;
-		} else {
-			return results.result;
-		}
-	}
-
-	context.exec = exec;
-
-	context.async = async;
-
-	context.setTimeout = setTimeout;
-	context.clearTimeout = clearTimeout;
-	context.setInterval = setInterval;
-	context.clearInterval = clearInterval;
-
-})(this);
-
-
-
-function asyncCall(c, func){
-	try{
-		var done = async();
-
-		var timer = setTimeout(function() {
+		let func = () => {
 			try{
-				func(c);
+				fn();
 			}
-			catch(err){
-				c.send(err);
+			catch (err) {
+
 			}
-			// do work
 			done(null, 'WORK DONE!');
-		}, 500);
+		}
+
+		let task = repeat ? this.setInterval(func, millis) : this.setTimeout(func, millis);
+
+		if(millis > 0){
+			this.phaser.awaitAdvanceInterruptibly(this.phaser.arrive(), millis, this.JTimeUnit.MILLISECONDS);
+		}
+		else{
+			this.phaser.awaitAdvanceInterruptibly(this.phaser.arrive());
+		}
+
+
+		this.tasks[taskName] = task;
+
+		return task;
 	}
-	catch(err){
-		c.send(err);
+
+	public static cancelTask(taskName:string){
+		if(this.tasks[taskName]){
+			this.clearInterval(this.tasks[taskName]);
+			this.tasks[taskName] = null;
+		}
 	}
 }
