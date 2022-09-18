a1515bf64b418a607b0cd7971e10800a64d0a9b9
dyamo <rikkanavo@gmail.com>
2022-09-18 22:44:46 +0400

    Update TL-Tasks 0.1.0: Add looped tasks.

--- TL-Tasks/TL-Tasks.js
+++ TL-Tasks/TL-Tasks.js
@@ -1,44 +1,53 @@
 /*! TL-Tasks
- * Version: 0.0.2
+ * Version: 0.1.0
  * https://github.com/TorayLife/mappet-TL-API/tree/master/TL-Tasks
  * Made by Dyamo (https://github.com/dyam0)
  */
+
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+
 var Task = /** @class */ (function () {
-    function Task(fn, delay, isFirst) {
-        if (delay === void 0) { delay = 0; }
-        if (isFirst === void 0) { isFirst = true; }
-        this.fn = fn;
-        this.delay = delay;
+    function Task() {
         this.nextTask = null;
-        if (isFirst) {
-            this.start(undefined);
-        }
     }
     Task.prototype.then = function (fn, delay) {
         if (delay === void 0) { delay = 0; }
-        this.nextTask = new Task(fn, delay, false);
+        this.nextTask = new DelayedTask(fn, delay, false);
+        return this.nextTask;
+    };
+    Task.prototype.thenLooped = function (fn, delay, count, initDelay) {
+        if (delay === void 0) { delay = 0; }
+        if (count === void 0) { count = 1; }
+        if (initDelay === void 0) { initDelay = delay; }
+        this.nextTask = new LoopedTask(fn, delay, count, initDelay, false);
         return this.nextTask;
     };
     Task.prototype.cancel = function () {
         this.nextTask = null;
     };
-    Task.prototype.start = function (previousResult) {
-        var _this = this;
-        Task.executorService.schedule(Task.makeRunnable(function () {
-            var result;
-            try {
-                result = _this.fn(_this);
-            }
-            catch (error) { }
-            if (_this.nextTask !== null && result !== null) {
-                _this.nextTask.result = result;
-                _this.nextTask.start(result);
-            }
-        }), this.delay, Task.TimeUnit.MILLISECONDS);
-    };
-    Task.define = function (fn, delay) {
+    Task.run = function (fn, delay) {
         if (delay === void 0) { delay = 0; }
-        return new Task(fn, delay);
+        return new DelayedTask(fn, delay);
+    };
+    Task.runLooped = function (fn, delay, count, initDelay) {
+        if (delay === void 0) { delay = 0; }
+        if (count === void 0) { count = 1; }
+        if (initDelay === void 0) { initDelay = delay; }
+        return new LoopedTask(fn, delay, count, initDelay);
     };
     Task.makeRunnable = function (fn) {
         return new (Java.extend(Java.type('java.lang.Runnable'), {
@@ -47,6 +56,86 @@ var Task = /** @class */ (function () {
     };
     Task.Executors = Java.type('java.util.concurrent.Executors');
     Task.TimeUnit = Java.type('java.util.concurrent.TimeUnit');
+    Task.UNLIMITED_LOOP = -42;
     Task.executorService = Task.Executors.newScheduledThreadPool(3);
     return Task;
 }());
+
+var DelayedTask = /** @class */ (function (_super) {
+    __extends(DelayedTask, _super);
+    function DelayedTask(fn, delay, isFirst) {
+        if (delay === void 0) { delay = 0; }
+        if (isFirst === void 0) { isFirst = true; }
+        var _this = _super.call(this) || this;
+        _this.fn = fn;
+        _this.delay = delay;
+        if (isFirst) {
+            _this.start();
+        }
+        return _this;
+    }
+    DelayedTask.prototype.start = function () {
+        var _this = this;
+        Task.executorService.schedule(Task.makeRunnable(function () {
+            var result;
+            try {
+                result = _this.fn(_this);
+            }
+            catch (error) {
+                _this.cancel();
+            }
+            if (_this.nextTask !== null) {
+                _this.nextTask.result = result;
+                _this.nextTask.start();
+            }
+        }), this.delay, Task.TimeUnit.MILLISECONDS);
+    };
+    return DelayedTask;
+}(Task));
+
+var LoopedTask = /** @class */ (function (_super) {
+    __extends(LoopedTask, _super);
+    function LoopedTask(fn, delay, iterationCount, initDelay, isFirst) {
+        if (delay === void 0) { delay = 0; }
+        if (iterationCount === void 0) { iterationCount = 0; }
+        if (initDelay === void 0) { initDelay = 0; }
+        if (isFirst === void 0) { isFirst = true; }
+        var _this = _super.call(this) || this;
+        _this.fn = fn;
+        _this.delay = delay;
+        _this.iterationCount = iterationCount;
+        _this.initDelay = initDelay;
+        _this.index = 0;
+        _this.isBroken = false;
+        if (isFirst) {
+            _this.start();
+        }
+        return _this;
+    }
+    LoopedTask.prototype.break = function () {
+        this.isBroken = true;
+    };
+    LoopedTask.prototype.start = function () {
+        var _this = this;
+        if (this.iterationCount <= 0 && this.iterationCount != Task.UNLIMITED_LOOP)
+            return;
+        var future = Task.executorService.scheduleWithFixedDelay(function () {
+            try {
+                _this.result = _this.fn(_this);
+            }
+            catch (error) {
+                _this.cancel();
+                future.cancel();
+            }
+            _this.index++;
+            if ((_this.index >= _this.iterationCount && _this.iterationCount != Task.UNLIMITED_LOOP) || _this.isBroken) {
+                if (_this.nextTask !== null) {
+                    _this.nextTask.result = _this.result;
+                    _this.nextTask.start();
+                }
+                future.cancel();
+            }
+        }, this.initDelay, this.delay, Task.TimeUnit.MILLISECONDS);
+    };
+    return LoopedTask;
+}(Task));
--- TL-Tasks/TL-Tasks.ts
+++ TL-Tasks/TL-Tasks.ts
@@ -1,55 +1,132 @@
 /*! TL-Tasks
- * Version: 0.0.2
+ * Version: 0.1.0
  * https://github.com/TorayLife/mappet-TL-API/tree/master/TL-Tasks
  * Made by Dyamo (https://github.com/dyam0)
  */
 
 type NonUndefined<T> = T extends undefined ? never : T;
 
-class Task<P extends NonUndefined<any>, R extends NonUndefined<any>> {
-	private static Executors = Java.type('java.util.concurrent.Executors');
-	private static TimeUnit = Java.type('java.util.concurrent.TimeUnit');
-	static executorService = Task.Executors.newScheduledThreadPool(3);
+abstract class Task<P extends NonUndefined<any>, R extends NonUndefined<any>> {
+    protected static Executors = Java.type('java.util.concurrent.Executors');
+    protected static TimeUnit = Java.type('java.util.concurrent.TimeUnit');
 
-	private nextTask: Task<R, any> | null = null;
-	result: R;
 
-	private constructor(private readonly fn: (task: Task<P | undefined, R>) => R, private readonly delay: number = 0, isFirst: boolean = true) {
-		if (isFirst) {
-			this.start(undefined);
-		}
-	}
+    static UNLIMITED_LOOP = -42
+    static executorService = Task.Executors.newScheduledThreadPool(3);
 
-	then<N>(fn: (task: Task<R, N>) => N, delay: number = 0): Task<R, N> {
-		this.nextTask = new Task<R, N>(fn, delay, false);
-		return this.nextTask;
-	}
 
-	cancel() {
-		this.nextTask = null;
-	}
+    protected nextTask: Task<R, any> | null = null;
+    abstract result: R
 
-	private start(previousResult: P | undefined) {
-		Task.executorService.schedule(Task.makeRunnable(() => {
-			let result;
-			try {
-				result = this.fn(this);
-			} catch (error) {}
 
-			if (this.nextTask !== null && result !== null) {
-				this.nextTask.result = result;
-				this.nextTask.start(result);
-			}
-		}), this.delay, Task.TimeUnit.MILLISECONDS);
-	}
+    then<N>(fn: (task: Task<R, N>) => N, delay: number = 0): Task<R, N> {
+        this.nextTask = new DelayedTask<R, N>(fn, delay, false);
+        return this.nextTask;
+    }
 
-	static define<P, R>(fn: (task: Task<P, R>) => R, delay: number = 0): Task<P, R> {
-		return new Task<P, R>(fn, delay);
-	}
+    thenLooped<N>(fn: (task: Task<R, N>) => N, delay: number = 0, count: number = 1, initDelay: number = delay): Task<R, N> {
+        this.nextTask = new LoopedTask<R, N>(fn, delay, count, initDelay, false);
+        return this.nextTask;
+    }
 
-	private static makeRunnable(fn: () => void) {
-		return new (Java.extend(Java.type('java.lang.Runnable'), {
-			run: fn,
-		}));
-	}
+    cancel() {
+        this.nextTask = null;
+    }
+
+
+    abstract start()
+
+
+    static run<P, R>(fn: (task: Task<P, R>) => R, delay: number = 0): Task<P, R> {
+        return new DelayedTask<P, R>(fn, delay);
+    }
+
+    static runLooped<P, R>(fn: (task: Task<P, R>) => R, delay: number = 0, count: number = 1, initDelay: number = delay): Task<P, R> {
+        return new LoopedTask<P, R>(fn, delay, count, initDelay);
+    }
+
+
+    protected static makeRunnable(fn: () => void) {
+        return new (Java.extend(Java.type('java.lang.Runnable'), {
+            run: fn,
+        }));
+    }
+}
+
+class DelayedTask<P extends NonUndefined<any>, R extends NonUndefined<any>> extends Task<P, R> {
+    result: R;
+
+    constructor(
+        private readonly fn: (task: Task<P | undefined, R>) => R,
+        private readonly delay: number = 0,
+        isFirst: boolean = true
+    ) {
+        super();
+        if (isFirst) {
+            this.start();
+        }
+    }
+
+    start() {
+        Task.executorService.schedule(Task.makeRunnable(() => {
+            let result;
+            try {
+                result = this.fn(this);
+            } catch (error) {
+                this.cancel();
+            }
+
+            if (this.nextTask !== null) {
+                this.nextTask.result = result;
+                this.nextTask.start();
+            }
+        }), this.delay, Task.TimeUnit.MILLISECONDS);
+    }
+}
+
+
+class LoopedTask<P extends NonUndefined<any>, R extends NonUndefined<any>> extends Task<P, R> {
+    result: R;
+    index: number = 0;
+    private isBroken: boolean = false;
+
+    constructor(
+        private readonly fn: (task: Task<P | undefined, R>) => R,
+        private readonly delay: number = 0,
+        private readonly iterationCount: number = 0,
+        private readonly initDelay: number = 0,
+        isFirst: boolean = true
+    ) {
+        super();
+        if (isFirst) {
+            this.start();
+        }
+    }
+
+    break() {
+        this.isBroken = true;
+    }
+
+    start() {
+        if (this.iterationCount <= 0 && this.iterationCount != Task.UNLIMITED_LOOP) return;
+
+        let future = Task.executorService.scheduleWithFixedDelay(() => {
+            try {
+                this.result = this.fn(this);
+            } catch (error) {
+                this.cancel();
+                future.cancel();
+            }
+
+            this.index++;
+
+            if ((this.index >= this.iterationCount && this.iterationCount != Task.UNLIMITED_LOOP) || this.isBroken) {
+                if (this.nextTask !== null) {
+                    this.nextTask.result = this.result;
+                    this.nextTask.start();
+                }
+                future.cancel();
+            }
+        }, this.initDelay, this.delay, Task.TimeUnit.MILLISECONDS);
+    }
 }
