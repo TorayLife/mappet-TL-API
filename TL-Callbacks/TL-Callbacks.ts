2b44024f767b79e54d5b60cd51a1b2e729ebf068
Toray-Life <stypmabichek@gmail.com>
2022-10-22 22:25:09 +0400

    Add TL-Callbacks

---
+++ TL-Callbacks/TL-Callbacks.ts
@@ -0,0 +1,29 @@
+class Callback {
+	static registry = {};
+	component: UIComponent;
+	function: Function = () => {};
+
+	constructor(id:string, component: UIComponent) {
+		this.component = component;
+		// @ts-ignore
+		Callback.registry[id] = this;
+	}
+
+	setFunction(func: Function) {
+		this.function = func;
+		return this;
+	}
+
+	static handler(c: IScriptEvent){
+		let context = c.player.UIContext;
+		let last = context.last;
+		if (last && Callback.registry[last]) {
+			let callback = Callback.registry[last];
+			callback.function(c, last);
+		}
+		if (last == '' && context.context && Callback.registry[context.context]) {
+			let callback = Callback.registry[context.context];
+			callback.function(c, last);
+		}
+	}
+}
